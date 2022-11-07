444ca3e9ef031a794aa531e094d06bec6910d26d
avishkakavindu <avishkakavindud@gmail.com>
2022-11-07 16:14:22 +0530

    Raw form validations

--- src/App.js
+++ src/App.js
@@ -9,11 +9,86 @@ import { Stylesheet } from './components/Stylesheet';
 import './appStyles.css';
 import styles from './appStyles.module.css';
 import { Form } from './components/Form';
+import { FormInput } from './components/FormInput';
+import { useState } from 'react';
 
 function App() {
+  const [values, setValues] = useState({
+    username: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+    label: 'Username',
+  });
+
+  const inputs = [
+    {
+      id: 1,
+      name: 'username',
+      type: 'text',
+      placeholder: 'username',
+      label: 'Username',
+      errorMessage: 'Username should be 3-16 characters',
+      pattern: '^[A-Za-z0-9]{3,16}',
+      required: true,
+    },
+    {
+      id: 2,
+      name: 'email',
+      type: 'email',
+      placeholder: 'email',
+      label: 'Email',
+      errorMessage: 'It should be a valid email.',
+      required: true,
+    },
+    {
+      id: 3,
+      name: 'password',
+      type: 'text',
+      placeholder: '',
+      label: 'Password',
+      errorMessage: 'Password should be 8-20 characters.',
+      pattern: `^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$`,
+      required: true,
+    },
+    {
+      id: 4,
+      name: 'confirmPassword',
+      type: 'text',
+      placeholder: '',
+      label: 'Confirm password',
+      errorMessage: "Password doesn't match.",
+      pattern: values.password,
+      required: true,
+    },
+  ];
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const onChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+
   return (
     <div className='App'>
-      <hr />
+      <form onSubmit={handleSubmit}>
+        <h3> Form with validations</h3>
+        {inputs.map((input) => {
+          return (
+            <FormInput
+              key={input.id}
+              {...input}
+              value={values[input.name]}
+              onChange={onChange}
+            />
+          );
+        })}
+
+        <button type='submit'>submit</button>
+      </form>
+      {/* <hr />
       props and children props
       <hr />
       <Greet name='Bruce' heroName='Batman'>
@@ -51,7 +126,7 @@ function App() {
       <hr />
       Forms
       <hr />
-      <Form />
+      <Form /> */}
     </div>
   );
 }
